6f96615b067274f4303444bda5aaab461e01f257
JC <131223025+RC8JCBanzuela@users.noreply.github.com>
2023-11-07 11:23:11 +0800

    Bid System

---
+++ biddingSystem2/biddingSystem2/Seller.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace biddingSystem2
+{
+    internal class Seller
+    {
+        public string sellerName;
+        public int itemPrice;
+        public string itemName;
+
+        public Seller(string sellerName, int itemPrice, string itemName)
+        {
+            this.sellerName = sellerName;
+            this.itemName = itemName;
+            this.itemPrice = itemPrice;
+        }
+    }
+}
---
+++ biddingSystem2/biddingSystem2/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace biddingSystem2
+{
+    internal class User
+    {
+        public string nameOfUser;
+        public int bidCost;
+
+        public User(string nameOfUser, int bidCost)
+        {
+            this.nameOfUser = nameOfUser;
+            this.bidCost = bidCost;
+        }
+    }
+}
